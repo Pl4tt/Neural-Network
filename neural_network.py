cfc65394ed3bcd6fb27536f901f287f5892a3f8c
unknown <dario.patt2@gmail.com>
2022-06-10 03:08:11 +0200

    add backpropagation and sgd

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 from typing import Iterable
+from mnist import MNIST
 
 
 class ActivationFunction:
@@ -13,14 +14,14 @@ class ActivationFunction:
 class Sigmoid(ActivationFunction):
     @staticmethod
     def forward(x):
-        if type(x) == float or type(x) == int:
+        if type(x) == float or type(x) == int or type(x) == np.float64:
             return 1/(1+math.exp(-x))
 
         return np.array(list(map(lambda element: Sigmoid.forward(element), x)))
 
     @staticmethod
     def derivative(x):
-        if type(x) == float or type(x) == int:
+        if type(x) == float or type(x) == int or type(x) == np.float64:
             return Sigmoid.forward(x)*(1-Sigmoid.forward(x))
 
         return np.array(list(map(lambda element: Sigmoid.derivative(element), x)))
@@ -28,14 +29,14 @@ class Sigmoid(ActivationFunction):
 class ReLU(ActivationFunction):
     @staticmethod
     def forward(x):
-        if type(x) == float or type(x) == int:
+        if type(x) == float or type(x) == int or type(x) == np.float64:
             return x if x > 0 else 0
 
         return np.array(list(map(lambda element: ReLU.forward(element), x)))
 
     @staticmethod
     def derivative(x):
-        if type(x) == float or type(x) == int:
+        if type(x) == float or type(x) == int or type(x) == np.float64:
             return 1 if x > 0 else 0
 
         return np.array(list(map(lambda element: ReLU.derivative(element), x)))
@@ -43,14 +44,14 @@ class ReLU(ActivationFunction):
 class Linear(ActivationFunction):
     @staticmethod
     def forward(x):
-        if type(x) == float or type(x) == int:
+        if type(x) == float or type(x) == int or type(x) == np.float64:
             return x
 
         return np.array(list(map(lambda element: Linear.forward(element), x)))
 
     @staticmethod
     def derivative(x):
-        if type(x) == float or type(x) == int:
+        if type(x) == float or type(x) == int or type(x) == np.float64:
             return 1
 
         return np.array(list(map(lambda element: Linear.derivative(element), x)))
@@ -58,14 +59,14 @@ class Linear(ActivationFunction):
 class Tanh(ActivationFunction):
     @staticmethod
     def forward(x):
-        if type(x) == float or type(x) == int:
+        if type(x) == float or type(x) == int or type(x) == np.float64:
             return math.tanh(x)
 
         return np.array(list(map(lambda element: Tanh.forward(element), x)))
 
     @staticmethod
     def derivative(x):
-        if type(x) == float or type(x) == int:
+        if type(x) == float or type(x) == int or type(x) == np.float64:
             return 1 - Tanh.forward(x)**2
 
         return np.array(list(map(lambda element: Tanh.derivative(element), x)))
@@ -73,7 +74,7 @@ class Tanh(ActivationFunction):
 
 class CostFunction:
     @staticmethod
-    def forward(predicted_y: np.ndarray, actual_y: np.ndarray) -> float: pass
+    def forward(predicted_y: np.ndarray, actual_y: np.ndarray) -> float | int: pass
 
     @staticmethod
     def derivative(predicted_y: np.ndarray, actual_y: np.ndarray) -> np.ndarray: pass
@@ -81,11 +82,11 @@ class CostFunction:
 class MeanSquaredError(CostFunction):
     @staticmethod
     def forward(predicted_y, actual_y):
-        return np.sum(np.square(np.subtract(predicted_y, actual_y)))/2
+        return np.sum(np.square(predicted_y - actual_y))/2
 
     @staticmethod
     def derivative(predicted_y, actual_y):
-        return np.subtract(predicted_y, actual_y)
+        return predicted_y - actual_y
 
 
 class Layer:
@@ -94,19 +95,34 @@ class Layer:
         self.input_shape = input_shape
         self.activation_function = activation_function
 
-        self.weights = np.random.rand(self.neuron_count, self.input_shape)
+        self.weights = np.random.rand(self.neuron_count, *self.input_shape)
         self.biases = np.random.rand(self.neuron_count)
 
-    def forward(self, input_arr: np.ndarray) -> np.ndarray:
+        self.set_update_arrays()
+    
+    def set_update_arrays(self) -> None:
+        self.update_weights = np.zeros((self.neuron_count, *self.input_shape))
+        self.update_biases = np.zeros((self.neuron_count,))
+
+    def forward(self, input_arr: np.ndarray, mode: int=1) -> np.ndarray:
         if self.input_shape != np.shape(input_arr):
             raise ValueError(f"The input shape is supposed to be {self.input_shape}. {np.shape(input_arr)} was given instead.")
         return input_arr
+    
+    def update_values(self, learning_rate: float) -> None:
+        self.weights -= learning_rate*self.update_weights
+        self.biases -= learning_rate*self.update_biases
+
+        self.set_update_arrays()
 
 class DenseLayer(Layer):
-    def forward(self, input_arr):
-        input_arr = super().forward(input_arr)
+    def forward(self, input_arr, mode=1):
+        input_arr = super().forward(input_arr, mode)
 
-        output_arr = np.dot(self.weights, input_arr) + self.biases
+        output_arr = self.weights@input_arr + self.biases
+        
+        if mode == 0:
+            return output_arr
 
         return self.activation_function.forward(output_arr)
 
@@ -128,25 +144,86 @@ class NeuralNetwork:
         
         self.input_shape = self.layers[0].input_shape
         self.output_shape = self.layers[-1].neuron_count
-    
-    def train_epoch(self, x: np.ndarray, y: np.ndarray, batch_size: int, learning_rate: float) -> None:
-        random_array = np.random.randint(0, len(x), size=batch_size)
-        baches = x[random_array,:]
 
+    def train_batch_sgd(self, mini_batch: np.ndarray, learning_rate: float) -> None:
+        for x, y in mini_batch:  # backpropagation / sgd
+            useful_y = np.zeros((10,))
+            useful_y[y-1] = 1
+
+            activation = x
+            activations = [x]
+            zs = []
+
+            for layer in self.layers:  # forward pass
+                z = layer.forward(activation, mode=0)
+                zs.append(z)
+                activation = layer.forward(activation)
+                activations.append(activation)
+
+            # backward pass
+            delta = self.cost_function.derivative(activations[-1], useful_y)*self.layers[-1].activation_function.derivative(zs[-1])
+            self.layers[-1].update_biases += delta
+            self.layers[-1].update_weights += np.atleast_2d(delta).T@np.atleast_2d(activations[-2])
+
+            for layer in range(2, len(self.layers)):
+                z = zs[-layer]
+                a_derivative = self.layers[-layer].activation_function.derivative(z)
+                delta = (self.layers[-layer+1].weights.T@delta)*a_derivative
+                self.layers[-layer].update_biases += delta
+                self.layers[-layer].update_weights += np.atleast_2d(delta).T@np.atleast_2d(activations[-layer-1])
+        
+        for layer in self.layers[1:]:  # updateing weights and biases
+            layer.update_values(learning_rate)
+
+    def train_epoch(self, datasets: np.ndarray, batch_size: int, learning_rate: float) -> None:
+        random_array = np.random.randint(0, len(datasets), size=batch_size)
+        mini_batch = datasets[random_array,:]
+
+        self.train_batch_sgd(mini_batch, learning_rate)
 
     def train(self, x: np.ndarray, y: np.ndarray, batch_size: int=None, learning_rate: float=0.03, epoch_count: int=100) -> None:
         if batch_size is None:
             batch_size = len(x)//epoch_count
+        
+        datasets = np.array(list(zip(x, y)), dtype=object)
 
         for i in range(epoch_count):
-            self.train_epoch(x, y, batch_size, learning_rate)
+            self.train_epoch(datasets, batch_size, learning_rate)
+
+            print(f"Epoch {i+1} finished")
+    
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        result = x
+
+        for layer in self.layers:
+            result = layer.forward(result)
+
+        return np.array(result)
 
 
 if __name__ == "__main__":
     neural_network = NeuralNetwork([
         InputLayer(784),
-        DenseLayer(16, 784, ReLU),
-        DenseLayer(16, 16, Sigmoid),
-        DenseLayer(10, 16, Sigmoid)
+        DenseLayer(16, (784,), ReLU),
+        DenseLayer(16, (16,), Sigmoid),
+        DenseLayer(16, (16,), Tanh),
+        DenseLayer(10, (16,), Sigmoid),
     ], MeanSquaredError)
+
+
+    mndata = MNIST("digit_data")
+
+    train_images, train_labels = mndata.load_training()
+    test_images, test_labels = mndata.load_testing()
+
+    train_images = mndata.process_images_to_numpy(train_images)
+    train_labels = mndata.process_images_to_numpy(train_labels)
+    test_images = mndata.process_images_to_numpy(test_images)
+    test_labels = mndata.process_images_to_numpy(test_labels)
     
+    prediction = neural_network.predict(np.array(train_images[0]))
+    
+    neural_network.train(train_images, train_labels, batch_size=500)
+
+    for image, label in zip(test_images[:30], test_labels[:30]):
+        print(np.argmax(neural_network.predict(image)), label)
