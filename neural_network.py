51c90e732455dc64ad4b9f14e8a5ab28dbc28788
unknown <dario.patt2@gmail.com>
2022-06-11 23:22:42 +0200

    Add calculations paper + nn improvments

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+import time
 from typing import Iterable
 from mnist import MNIST
 
@@ -77,9 +77,13 @@ class Layer:
         self.input_shape = input_shape
         self.activation_function = activation_function
 
-        self.weights = np.random.rand(self.neuron_count, *self.input_shape)
-        self.biases = np.random.rand(self.neuron_count)
-
+        # self.weights = np.random.rand(self.neuron_count, *self.input_shape)
+        # self.biases = np.random.rand(self.neuron_count)
+        self.weights = np.random.uniform(0, 0.0001, size=(self.neuron_count, *self.input_shape))
+        self.biases = np.random.uniform(0, 0.0001, size=(self.neuron_count,))
+        
+        # print("W", self.weights)
+        # print("B", self.biases)
         self.set_update_arrays()
     
     def set_update_arrays(self) -> None:
@@ -94,7 +98,7 @@ class Layer:
     def update_values(self, learning_rate: float) -> None:
         self.weights -= learning_rate*self.update_weights
         self.biases -= learning_rate*self.update_biases
-
+        # print(self.update_weights, self.update_biases)
         self.set_update_arrays()
 
 class DenseLayer(Layer):
@@ -143,19 +147,25 @@ class NeuralNetwork:
                 activations.append(activation)
 
             # backward pass
+            # print("zs", zs[-1])
+            # print("D", self.cost_function.derivative(activations[-1], useful_y), self.layers[-1].activation_function.derivative(zs[-1]))
             delta = self.cost_function.derivative(activations[-1], useful_y)*self.layers[-1].activation_function.derivative(zs[-1])
+            # print("B", delta, "W", np.atleast_2d(delta).T@np.atleast_2d(activations[-2]))
             self.layers[-1].update_biases += delta
-            
-            # self.layers[-1].update_weights += np.atleast_2d(delta).T@np.atleast_2d(activations[-2])
-            self.layers[-1].update_weights += zs[-1].T@delta
+            self.layers[-1].update_weights += np.atleast_2d(delta).T@np.atleast_2d(activations[-2])
 
             for layer in range(2, len(self.layers)):
+                # print("zs", zs[-layer])
                 a_derivative = self.layers[-layer].activation_function.derivative(zs[-layer])
+                # print("F", self.layers[-layer+1].weights.T@delta, "S", a_derivative)
                 delta = (self.layers[-layer+1].weights.T@delta)*a_derivative
-                self.layers[-layer].update_biases += delta.reshape((self.layers[-layer].neuron_count,))
+                # print("B", delta, "W", np.atleast_2d(delta).T@np.atleast_2d(activations[-layer-1]))
+                self.layers[-layer].update_biases += delta
                 self.layers[-layer].update_weights += np.atleast_2d(delta).T@np.atleast_2d(activations[-layer-1])
 
         for layer in self.layers[1:]:  # updateing weights and biases
+            layer.update_biases /= len(mini_batch)
+            layer.update_weights /= len(mini_batch)
             layer.update_values(learning_rate)
 
     def train_epoch(self, datasets: np.ndarray, batch_size: int, learning_rate: float) -> None:
@@ -164,7 +174,7 @@ class NeuralNetwork:
 
         self.train_batch_sgd(mini_batch, learning_rate)
 
-    def train(self, x: np.ndarray, y: np.ndarray, batch_size: int=None, learning_rate: float=0.03, epoch_count: int=100) -> None:
+    def train(self, x: np.ndarray, y: np.ndarray, test: tuple[np.ndarray], batch_size: int=None, learning_rate: float=0.03, epoch_count: int=100) -> None:
         if batch_size is None:
             batch_size = len(x)//4
         
@@ -174,12 +184,14 @@ class NeuralNetwork:
             self.train_epoch(datasets, batch_size, learning_rate)
 
             print(f"Epoch {i+1} finished")
+            # print(self.predict(test[0]), test[1])
     
     def predict(self, x: np.ndarray) -> np.ndarray:
         result = x
-
+        # print(x)
         for layer in self.layers:
             result = layer.forward(result)
+            # print(result)
 
         return np.array(result)
 
@@ -188,9 +200,9 @@ if __name__ == "__main__":
     neural_network = NeuralNetwork([
         InputLayer(784),
         DenseLayer(500, (784,), ReLU),
-        DenseLayer(50, (500,), ReLU),
-        DenseLayer(20, (50,), Tanh),
-        DenseLayer(10, (20,), Sigmoid),
+        DenseLayer(200, (500,), Sigmoid),
+        DenseLayer(100, (200,), Linear),
+        DenseLayer(10, (100,), Sigmoid),
     ], MeanSquaredError)
 
 
@@ -206,10 +218,12 @@ if __name__ == "__main__":
     
     prediction = neural_network.predict(np.array(train_images[0]))
 
-    neural_network.train(train_images, train_labels, batch_size=1000, epoch_count=20)
-
-    for image, label in zip(test_images[:30], test_labels[:30]):
-        print((neural_network.predict(image)), label)
+    neural_network.train(train_images, train_labels, (test_images[0], test_labels[0]), batch_size=1000, epoch_count=40)
+    # neural_network.predict(test_images[1])
+    # print(test_labels[1])
+    for image, label in zip(test_images[:10], test_labels[:10]):
+        prediction = neural_network.predict(image)
+        print(prediction, np.argmax(prediction), label)
     
     # for layer in neural_network.layers[1:]:
     #     print(layer.weights, layer.biases)
